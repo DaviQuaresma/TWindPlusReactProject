36562feca55d87ef6189b10d5e9944ececb73b4e
Davi Quaresma da Silva Henriques <quaresma.davi@gmail.com>
2024-10-02 17:37:31 -0300

    .

---
+++ TAILWIND UI PROJECT/src/components/EmailContent/EmailData.jsx
@@ -0,0 +1,9 @@
+export default function EmailData({ children }) {
+    return(
+        <div className="
+            text-gray-600 dark:text-gray-400
+        ">
+            {children}
+        </div>
+    )
+}
---
+++ TAILWIND UI PROJECT/src/components/EmailContent/EmailTitle.jsx
@@ -0,0 +1,5 @@
+export default function EmailTitle({children}) {
+    return(
+        <h2 className="text-2x1 font-bold mb-2">{children}</h2>
+    )
+}
